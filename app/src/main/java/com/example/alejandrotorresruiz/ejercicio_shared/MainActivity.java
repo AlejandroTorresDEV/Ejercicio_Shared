ea43d2541ae9ee3ea5342d8e1e066e375fc00a60
alejandrotorresruiz <Pollete456>
2017-11-11 21:48:58 +0100

    Prueba

---
+++ app/src/main/java/com/example/alejandrotorresruiz/ejercicio_shared/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alejandrotorresruiz.ejercicio_shared;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
